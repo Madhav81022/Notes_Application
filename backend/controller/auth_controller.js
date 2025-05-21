061e4164b9bc7796f5c420a5b7a0bd3c40f36c08
Madhav kumar <kumar.madhav810222@gmail.com>
2025-05-21 16:39:02 +0530

    routess

--- backend/controller/auth_controller.js
+++ backend/controller/auth_controller.js
@@ -61,7 +61,7 @@ export const signin = async(req,res,next)=>{
 
         const {password: pass, ...rest} = validUser._doc;
 
-        res.cookie("access_token",token,{httpOnly:true, sameSite:"none", secure:false}).status(200).json({
+        res.cookie("access_token",token,{httpOnly:true, sameSite:"none", secure:true}).status(200).json({
             success:true,
             message:"Login Successful!",
             rest,
